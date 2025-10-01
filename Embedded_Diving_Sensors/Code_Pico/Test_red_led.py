7fe673b30f9605326c158078b87eb9994d1e7f15
CletBoudehenn <clet.boudehenn@net-c.fr>
2025-10-01 21:49:55 +0200

    Add Embedded_Diving Sensor

---
+++ Embedded_Diving_Sensors/Code_Pico/Test_red_led.py
@@ -0,0 +1,14 @@
+"""
+// Update: 16. Aug 2023
+"""
+
+from machine import Pin
+import time
+led_rouge = Pin(15, Pin.OUT)
+led_blue = Pin(14, Pin.OUT)
+while True:
+ led_rouge.toggle()
+ time.sleep(0.5)
+ led_blue.toggle()
+ time.sleep(0.5)
+ 
